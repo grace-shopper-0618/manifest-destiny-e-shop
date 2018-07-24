8622bd865adc2d50fd7c496f2eb585652ca3451b
Shelby Smitherman <smithermansl@gmail.com>
2018-07-24 15:29:03 -0400

    associations made

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,18 +1,25 @@
 const User = require('./user')
+const Product = require('./product')
+const Review = require('./reviews')
+const LineItem = require('./lineItem')
+const Order = require('./order')
+const Category = require('./category')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
+Product.belongsToMany(Order, {through: 'line-item'})
+Order.belongsToMany(Product, {through: 'line-item'})
 
-/**
- * We'll export all of our models here, so that any time a module needs a model,
- * we can just require it from 'db/models'
- * for example, we can say: const {User} = require('../db/models')
- * instead of: const User = require('../db/models/user')
- */
+Order.belongsTo(User)
+User.hasMany(Order)
+
+Product.belongsToMany(Category)
+Category.belongsToMany(Order)
+
+Review.belongsTo(User)
+User.hasMany(Review)
+
+Review.belongsTo(Product)
+Product.hasMany(Review)
+
 module.exports = {
-  User
+  User, Product, Review, LineItem, Order, Category
 }
