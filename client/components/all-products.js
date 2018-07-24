d796a6ac7a526fc687c0558ce82be82b47af920d
Amy Berg <aberg2014@gmail.com>
2018-07-24 17:49:17 -0400

    allproducts view

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -4,30 +4,40 @@ import {getProductsFromDb} from '../store/products'
 
 // COMPONENT
 class AllProducts extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      products: []
-    }
-  }
 
   componentDidMount () {
-    const products = this.props.fetchProducts()
-    this.setState({
-      products
-    })
+    this.props.fetchProducts()
   }
 
   render () {
-    return (<div>All products!</div>)
+    const { products } = this.props
+    return (
+      <div id="all-products">
+        {
+          products.map(product => (
+            <div className="product-card" >
+              <h3>{product.title}</h3>
+              <img className="product-card-image" src={product.photoUrl} />
+              {/* does product.price give the correct price? also show part of description? show 'sold out' or similar comments to shopper based on inventory? */}
+              <h4>${product.price}</h4>
+              <h4>Number in stock: {product.inventory}</h4>
+            </div>
+          ))
+        }
+      </div>
+    )
   }
 }
 
-// add mapstate if neccessary?
+const mapState = state => ({
+  products: state.products
+})
 
 const mapDispatch = (dispatch) => ({
   fetchProducts: () => dispatch(getProductsFromDb())
 })
 
-export default connect(null, mapDispatch)(AllProducts)
+export default connect(mapState, mapDispatch)(AllProducts)
+
+
 
