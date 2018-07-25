c86b76dc5bca51096529b5446cafc53914f5ada9
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-07-25 12:53:55 -0400

    refactor(models): Adds isActiveCart column to Order model

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Order = db.define('order')
+const Order = db.define('order', {
+  isActiveCart: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
 
 module.exports = Order
