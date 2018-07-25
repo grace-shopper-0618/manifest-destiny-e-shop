3e2f9869371b515c9bc12adb29f0a42e135c2cd6
Amy Berg <aberg2014@gmail.com>
2018-07-25 13:04:49 -0400

    apiroute(categories) created categories api route with a get request for all categories

---
+++ server/api/categories.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const {Category} = require('../db/models')
+module.exports = router
+
+//get for path /api/categories
+router.get('/', async (req, res, next) => {
+  try {
+      const categories = await Category.findAll()
+      res.status(200).json(categories)
+  } catch (err) {
+      next(err)
+  }
+})
