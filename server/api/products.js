cd18e1947623d559b33a66aff57bf393feda2706
almondmilk96 <kara.hirschman@gmail.com>
2018-07-25 20:30:10 -0400

    product routes testing

--- server/api/products.js
+++ server/api/products.js
@@ -2,33 +2,77 @@ const router = require('express').Router()
 const {Product, Category, Review} = require('../db/models')
 module.exports = router
 
-//get all products
+//get all products from the db
 router.get('/', async (req, res, next) => {
     try {
         const products = await Product.findAll({
           include: [{model: Category}]
         })
         res.status(200).json(products)
-        // do we need special error handling here?
+        if (!products) {
+          const err = new Error('Unable to find any products.')
+          err.sendStatus(404) //should this be a different error code? it will throw if multiple filters are applied or the db is empty
+        }
     } catch (err) {
         next(err)
     }
 })
 
-//get single product
+//get single product from the db
 router.get('/:id', async (req, res, next) => {
   try {
-    const id = req.params.id,
-    product = await Product.findById(id, {
+    const id = +req.params.id
+    const product = await Product.findById(id, {
       include: [{model: Category}, {model: Review}]
     })
-
-    if(!product) {
-      const err = new Error(`No product found with id of ${id}.`)
+    if (!product) {
+      const err = new Error(`No product with id ${id} for sale.`)
       err.status = 404
       return next(err)
     }
     res.status(200).json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//add a product to the db
+router.post('/', async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body)
+    if (!product) {
+      const err = new Error('Unable to create product.')
+      err.status = 406
+      return next(err)
+    }
+    res.status(201).json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//update a product in the db
+router.put('/:id', async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id)
+    if (!product) {
+      const err = new Error(`Unable to find product with id of ${req.params.id}`)
+      err.status = 404
+      return next(err)
+    }
+    await product.update(req.body)
+    res.status(200).json(product)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//delete a product from the db
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    await Product.destroy({where: {id}})
+    res.sendStatus(204)
   } catch (err) {
     next(err)
   }
--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -11,21 +11,61 @@ describe('Product routes', () => {
 
   describe('/api/products/', () => {
 
-    beforeEach(() => {
-      return Product.create({
-        title: 'Horseshoes',
-        price: 999,
-        inventory: 10
-      })
-    })
-
-    it('GET /api/products', async () => {
+    it('GET /api/products/', async () => {
       const res = await request(app)
-        .get('/api/products')
+        .get('/api/products/')
         .expect(200)
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].title).to.be.equal('Horseshoes')
+      expect (res.body).to.be.an('array')
+    })
+
+    it('GET /api/products/:id', async () => {
+      const res = await request(app)
+        .get('/api/products/8')
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.title).to.be.equal('Palomino Horse')
+    })
+
+    it('DELETE /api/products', async () => {
+      await request(app)
+        .put('/api/products/8')
+        .expect(204)
+    })
+    
+    it('POST /api/products/', async () => {
+      const newProduct = Product.create({
+          title: 'Set of four (4) horseshoes',
+          price: 799,
+          inventory: 50,
+          description: 'Four (4) certified lucky iron horseshoes'
+        })
+      const res = await request(app)
+        .post('/api/products/', newProduct)
+        .expect(201)
+      expect(res.body).to.be.an('object')
+      expect(res.title).to.equal('Set of four (4) horseshoes')
+      expect(res.description).to.be.equal('Four (4) certified lucky iron horseshoes')
+    })
+
+    it('GET /api/products/:id', async () => {
+      const res = await request(app)
+        .get('/api/products/8')
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.title).to.be.equal('Set of Four Horsehoes')
+    })
+
+    it('PUT /api/products', async () => {
+      const res = await request(app)
+        .put('/api/products/8', {
+          description: 'Sack of Potatoes.'
+        })
+        .expect(200)
+      expect(res.body).to.be.an.an('object')
+      expect(res.description).to.equal('Starches are an important part of a healthy diet.')
     })
   }) // end describe('/api/products')
 }) // end describe('Product routes')
