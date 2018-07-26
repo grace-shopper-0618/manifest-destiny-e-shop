3c6a0e103928ebe2b6ee4b38f5eb4e481c2e825d
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-07-26 11:13:55 -0400

    feat(specs): Adds commented-out product model specs

--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -9,29 +9,28 @@
 //   })
 
 //   describe('Validations', () => {
-//     it('requires title', async () => {
-//       const product = Product.create()
-//       try {
-//         await product.validate()
-//         throw Error('validation was successful but should have failed without name')
-//       }
-//       catch (err) {
-//         expect(err.message).to.contain('name cannot be null')
-//       }
-//     })
-//     it('successfully creates new instance', async () => {
-//       const product = await Product.create({
-//         title: 'oxen',
-//         price: 40,
-//         inventory: 2,
-//       })
-//       await product.validate()
-//       expect(product).to.equal({
-//         title: 'oxen',
-//         price: 40,
-//         inventory: 2,
-//       })
-
+//     // it('requires title', async () => {
+//     //   const product = Product.create()
+//     //   try {
+//     //     await product.validate()
+//     //     throw Error('validation was successful but should have failed without name')
+//     //   }
+//     //   catch (err) {
+//     //     expect(err.message).to.contain('name cannot be null')
+//     //   }
+//     // })
+//     // it('successfully creates new instance', async () => {
+//     //   const product = await Product.create({
+//     //     title: 'oxen',
+//     //     price: 40,
+//     //     inventory: 2,
+//     //   })
+//     //   await product.validate()
+//     //   expect(product).to.equal({
+//     //     title: 'oxen',
+//     //     price: 40,
+//     //     inventory: 2,
+//     //   })
 //     })
 //   })
 // })
