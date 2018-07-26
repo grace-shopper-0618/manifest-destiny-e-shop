2f33336b39a89d0497a7410f63d866ec2f6c855a
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-07-26 12:38:23 -0400

    feat(form) Adds more updates to product form component

--- client/components/product-form.js
+++ client/components/product-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { editProductInDb } from '../store/product'
 
 class ProductForm extends Component {
   constructor() {
@@ -11,30 +12,48 @@ class ProductForm extends Component {
       inventory: 0,
       photoUrl: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+
+  }
+
+
   render() {
     return (
       <div>
-        <form>
+        <form id="product-form" onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="title">Title</label>
-            <input name="title" type="text" />
+            <input name="title" type="text" onChange={this.handleChange} value={this.state.title} />
           </div>
           <div>
             <label htmlFor="description">Description</label>
-            <input name="description" type="text" />
+            <input name="description" type="text" onChange={this.handleChange} value={this.state.description} />
           </div>
           <div>
             <label htmlFor="price">Price</label>
-            <input name="price" type="text" />
+            <input name="price" type="text" onChange={this.handleChange} value={this.state.price} />
           </div>
           <div>
             <label htmlFor="inventory">Inventory</label>
-            <input name="inventory" type="text" />
+            <input name="inventory" type="text" onChange={this.handleChange} value={this.state.inventory} />
           </div>
           <div>
             <label htmlFor="photoUrl">Photo Url</label>
-            <input name="photoUrl" type="text" />
+            <input name="photoUrl" type="text" onChange={this.handleChange} value={this.state.photoUrl} />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
           </div>
         </form>
       </div>
@@ -42,3 +61,25 @@ class ProductForm extends Component {
   }
 
 }
+
+const mapEdit = state => {
+  return {
+
+  }
+}
+
+const mapAdd = state => {
+  return {
+
+  }
+
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    editProductInDb: (product, history) => dispatch(editProductInDb(product, history))
+  }
+}
+
+export const EditForm = connect(mapEdit, mapDispatch)(ProductForm)
+export const AddForm = connect(mapAdd, mapDispatch)(ProductForm)
