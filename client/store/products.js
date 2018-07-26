bd7b212c7765b9440e6703132983775442fe3d32
Amy Berg <aberg2014@gmail.com>
2018-07-26 11:32:35 -0400

    store(products) added delete action and functionality to store

--- client/store/products.js
+++ client/store/products.js
@@ -3,6 +3,7 @@ import history from '../history'
 
 //ACTION TYPES
 const GET_PRODUCTS = 'GET_PRODUCTS'
+const DELETE_PRODUCT = 'DELETE_PRODUCT'
 
 //INITIAL STATE
 const initialState = []
@@ -11,6 +12,11 @@ const initialState = []
 const getProducts = (products) => ({
     type: GET_PRODUCTS,
     products
+})
+
+const deleteProduct = (productId) => ({
+  type: DELETE_PRODUCT,
+  productId
 })
 
 //THUNK CREATORS
@@ -25,11 +31,24 @@ export const getProductsFromDb = () => {
   }
 }
 
+export const removeProductFromDb = (productId) => {
+  return async (dispatch) => {
+    try {
+      await axios.delete(`/api/products/${productId}`)
+      dispatch(deleteProduct(productId))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
 //REDUCER
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_PRODUCTS:
             return action.products
+        case DELETE_PRODUCT:
+            return action.products.filter(product => product.id !== action.productId)
         default:
             return state
     }
