87f6c9d4d8aac645897f51792a00e0953e4ac145
Shelby Smitherman <smithermansl@gmail.com>
2018-07-27 15:43:33 -0400

    feat(reviews): display user reviews on user home page

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -7,10 +7,23 @@ import {connect} from 'react-redux'
  */
 export const UserHome = props => {
   const {email} = props
-
+  const {reviews} = props.user
+ 
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
+    <div id="userHome">
+      <h2>Welcome, {email}</h2>
+      <div id="userReviews">
+        <h4>Your Reviews</h4>
+        {
+          reviews.map(review => {
+            return (
+              <div id="review" key={review.id}>
+                <p>{review.text}</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
@@ -20,7 +33,8 @@ export const UserHome = props => {
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    email: state.user.email,
+    user: state.user
   }
 }
 
