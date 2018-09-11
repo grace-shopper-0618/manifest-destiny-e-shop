d8497b43781c2efc67dca6e39ff905a94f1e1cdf
Alicia Dinkel <alicia.dinkel@gmail.com>
2018-09-11 18:26:47 -0400

    styles sidebar

--- client/components/category-list.js
+++ client/components/category-list.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { setSelectedCategory, deselectCategory } from '../store/selectedCategory';
 import { getCategoriesFromDb } from '../store/categories';
 
 class CategoryList extends React.Component {
 
-  componentDidMount () {
+  componentDidMount() {
     this.props.fetchCategories()
   }
 
-  render () {
+  render() {
     const { categories, selectedCategory, resetCategory, setCategory } = this.props
     return (
       <div id='categoryList'>
         <ul id='category-list'>
-          <li className='title'>Displaying:</li>
-          <li>{selectedCategory.name ? selectedCategory.name : 'All Products'}</li>
-          { selectedCategory.name? <li onClick={resetCategory} > Show All </li> : <li />}
-          <li className='title'> Filter by: </li>
+          <li className='selected-category'>{selectedCategory.name ? selectedCategory.name : 'All Products'}</li>
+
+          <li id='filter-by'> Filter by Category: </li>
+          {selectedCategory.name ? <li onClick={resetCategory} className='category-item'> Show All </li> : <li />}
           {
             categories && categories.map(category => (
-              <li key={category.id} onClick={() => setCategory(category.id)} >{category.name}</li>
+              <li key={category.id} onClick={() => setCategory(category.id)} className='category-item' >{category.name}</li>
             ))
           }
         </ul>
